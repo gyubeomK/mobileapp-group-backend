8640f17c29b033235f3b5315760452e690d1ab41
Tai Nguyen <tainguyen2101@gmail.com>
2020-11-23 13:53:19 -0800

    update add friend api, 2 means request pending

--- routes/contact.js
+++ routes/contact.js
@@ -400,6 +400,7 @@ router.post("/add", (request, response, next) => {
 }, (request, response) => {
     let check = 'SELECT * FROM Contacts WHERE MemberID_A = $1 AND MemberID_B = (SELECT MemberID from Members WHERE Username = $2)'
     let query = 'INSERT INTO Contacts (MemberID_B, MemberID_A) VALUES ($1, (SELECT MemberID from Members WHERE Username = $2))'
+    let query2 = 'INSERT INTO Contacts (MemberID_A, MemberID_B, Verified) VALUES ($1, (SELECT MemberID from Members WHERE Username = $2), 2)'
     let values = [request.decoded.memberid, request.body.userName]
 
     pool.query(check, values).then(result => {
@@ -409,7 +410,8 @@ router.post("/add", (request, response, next) => {
             })
         } else {
             pool.query(query, values).then(result => {
-               
+                pool.query(query2, values)
+    
                 response.send({
                     success: true
                 })
