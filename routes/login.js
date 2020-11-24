83bda4b5eed524cb82efda8534cf81d4466ee702
GyubeomKim <gyubeom@uw.edu>
2020-11-23 22:02:18 -0800

    changed api

--- routes/login.js
+++ routes/login.js
@@ -75,7 +75,7 @@ router.get('/', (request, response) => {
                         },
                         config.secret,
                         { 
-                            expiresIn: '14 days' // expires in 14 days
+                            expiresIn: '365 days' // expires in 14 days
                         }
                     )
                     //package and send the results
