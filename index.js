33896693c83076ce8158617447dca57c3573271b
GyubeomKim <gyubeom@uw.edu>
2020-11-23 22:09:13 -0800

    new api

--- index.js
+++ index.js
@@ -30,8 +30,6 @@ app.use('/fiveday', require('./routes/fiveday.js'))
 
 app.use('/twelvehour', require('./routes/twelvehour.js'))
 
-
-
 /*
  * This middleware function will respond to inproperly formed JSON in 
  * request parameters.
