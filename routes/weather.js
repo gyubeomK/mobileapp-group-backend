bdc9bea18471b452e0bad90339adc382956663a7
Tai Nguyen <tainguyen2101@gmail.com>
2020-12-04 15:59:24 -0800

    test

--- routes/weather.js
+++ routes/weather.js
@@ -8,7 +8,7 @@ router.get('/', function(request, response) {
     .header("X-RapidAPI-Key", "4165b5da19msh442263a57547cdap159f3cjsna1fc6e6389b6")
     .header("x-rapidapi-host", "community-open-weather-map.p.rapidapi.com")
     .query({
-        'appid' : '87778b361ce442fe8501b305005e67c6',
+        'appid' : 'default-application_4891802',
         'lon': '12.4924',
         'lat': '41.8902',
         'units': 'metric'
