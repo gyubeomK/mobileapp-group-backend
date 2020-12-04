189e951576ff6c10ee4cafdeb2e536a3628f564a
Tai Nguyen <tainguyen2101@gmail.com>
2020-12-04 15:46:00 -0800

    test

--- routes/weather.js
+++ routes/weather.js
@@ -14,6 +14,7 @@ router.get('/', function(request, response) {
         'units': 'metric',
     })
     .end(function(result) {
+        response.writeHead(200, {"Content-Type": "JSON"});
         response.write(result.body);
     })
 })
