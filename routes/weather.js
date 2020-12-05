27acfa4518adfe255a27411d5fd97ef00931bba8
Tai Nguyen <tainguyen2101@gmail.com>
2020-12-04 17:38:47 -0800

    add an api to get current weather using city name

--- routes/weather.js
+++ routes/weather.js
@@ -1,13 +1,24 @@
+d/**
+ * Weather API at https://api.openweathermap.org
+ * @author Ford Nguyen
+ * @version 1.0
+ */
 const express = require('express')
 const https = require('https')
 const API_KEY = '87778b361ce442fe8501b305005e67c6'
 
 var router = express.Router()
 const bodyParser = require('body-parser')
-router.use(bodyParser.urlencoded({extended: true}))
-
+router.use(bodyParser.urlencoded({extended: true})/**
+ * @api {get} /?:city=<city>
+ * @apiName getWeatherCurrentUsingCity
+ * @apiGroup Weather
+ * @apiDescription Connect to a 3rd party API using https and get the current weather with city name provided
+ * @apiSuccess weather data
+ * @apiError (400: Missing Required Information) {String} message "Missing required information"
+ */
 router.get("/", (request, response) => {
-    const query = request.query.name
+    const query = request.query.city
     console.log(query)
     let unit = "imperial"
     const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&units="+unit+"&appid="+API_KEY
