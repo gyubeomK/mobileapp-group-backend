08b1130f4d36efc81bb411205731a019900448c6
GyubeomKim <gyubeom@uw.edu>
2020-12-05 21:14:50 -0800

    edit

--- routes/chats.js
+++ routes/chats.js
@@ -377,4 +377,80 @@ router.delete("/:chatId/:email", (request, response, next) => {
     }
 )
 
+
+router.put("/:chatId/:memberId", (request, response, next) => {
+    //validate on empty parameters
+    if (!request.params.chatId || !request.params.memberid) {
+        response.status(400).send({
+            message: "Missing required information"
+        })
+    }  else if (isNaN(request.params.chatId)) {
+        response.status(400).send({
+            message: "Malformed parameter. chatId must be a number"
+        })
+    } else {
+        next()
+    }
+}, (request, response, next) => {
+    //validate chat id exists
+    let query = 'SELECT * FROM CHATS WHERE ChatId=$1'
+    let values = [request.params.chatId]
+
+    pool.query(query, values)
+        .then(result => {
+            if (result.rowCount == 0) {
+                response.status(404).send({
+                    message: "Chat ID not found"
+                })
+            } else {
+                next()
+            }
+        }).catch(error => {
+            response.status(400).send({
+                message: "SQL Error",
+                error: error
+            })
+        })
+},  (request, response, next) => {
+        //validate emember id does not already exist in the chat
+        let query = 'SELECT MemberID FROM ChatMembers WHERE ChatId=$1 AND MemberId =$2'
+        let values = [request.params.chatId, request.params.memberid]
+    
+        pool.query(query, values)
+            .then(result => {
+                if (result.rowCount > 0) {
+                    response.status(400).send({
+                        message: "user already joined"
+                    })
+                } else {
+                    next()
+                }
+            }).catch(error => {
+                response.status(400).send({
+                    message: "SQL Error",
+                    error: error
+                })
+            })
+
+}, (request, response) => {
+    //Insert the memberId into the chat
+    let insert = `INSERT INTO ChatMembers(ChatId, MemberId)
+                  VALUES ($1, $2)
+                  RETURNING *`
+    let values = [request.params.chatId, request.params.memberid]
+    pool.query(insert, values)
+        .then(result => {
+            response.send({
+                sucess: true
+            })
+        }).catch(err => {
+            response.status(400).send({
+                message: "SQL Error",
+                error: err
+            })
+        })
+    }
+)
+
+
 module.exports = router
