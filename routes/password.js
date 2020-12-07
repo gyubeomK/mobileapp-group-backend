1573afa78fdc94f7884657a6ec690d4db55342a1
GyubeomKim <gyubeom@uw.edu>
2020-12-07 00:48:56 -0800

    added password changing and it works

--- routes/password.js
+++ routes/password.js
@@ -9,66 +9,63 @@ let pool = require('../utilities/utils').pool
 var router = express.Router()
 
 const bodyParser = require("body-parser")
+const { nextTick } = require('process')
 
 let getHash = require('../utilities/utils').getHash
+
 //This allows parsing of the body of POST requests, that are encoded in JSON
 router.use(bodyParser.json())
 
-router.post('/', (request, response)=>{
-    let email = request.body.email
-    let oldPassword = request.body.oldPassword
-    let newPassword = equest.body.newPassword
-    if (!email || !oldPassword|| !newPassword) {
-        response.status(400).send({
-            message: "Missing required information"
+router.post('/', (request, response, next)=> {
+    var email = request.body.email
+    var oldpass = request.body.oldpassword
+    var newpass = request.body.newpassword
+    if(!email || !oldpass || !newpass) {
+        response.status(404).send({
+            message: 'Missing Information'
         })
     } else {
         next()
     }
 }, (request, response, next) => {
-    //validate chat id exists
     let query = 'SELECT Password, Salt FROM Members WHERE Email=$1'
-    let values = [email]
-
+    let values = [request.body.email]
     pool.query(query, values)
         .then(result => {
             if (result.rowCount == 0) {
                 response.status(404).send({
                     message: "User Information not found"
                 })
+                return
+            } 
+            let salt = result.rows[0].salt
+            let curpassword = result.rows[0].password
+            let TheirSalt = getHash(request.body.oldpassword, salt)
+            let newSalt = crypt.randomBytes(32).toString('hex')
+            let MySalt = getHash(request.body.newpassword, newSalt) //hash for new password
+            let update =`UPDATE Members 
+                         SET Password=$1, Salt=$2 WHERE email=$3`
+            let values = [request.body.oldpassword, request.body.newpassword, request.body.email]
+            if(TheirSalt === curpassword) {
+                console.log('Credentials Match!')
+                pool.query(update, values)
+                .then(result => {
+                    if(result.rowCount > 0) {
+                        response.send({
+                        message: "New Password is UPDATED!"
+                        })
+                    } else {
+                        response.status(400).send({
+                            message: 'Password is not able to be Changed!'
+                        })
+                    }
+                }) 
             } else {
-                next()
-            }
-        }).catch(error => {
-            response.status(400).send({
-                message: "SQL Error",
-                error: error
-            })
-        })
-}, (request, response) => {
-    let ThierSalt = getHash(oldPassword, result.row[0].salt)
-
-    if(ThierSalt == result.row[0].salt) {
-        let newSalt = crypt.randomBytes(32).toString('hex')
-        let hashNewPass = getHash(newPassword, newSalt)
-        let insert = `UPDATE MEMBERS
-                      SET Password = $1, Salt = $2 WHERE Email = $3
-                      RETURNING *`
-        let values = [newSalt, hashNewPass, email]
-        pool.query(insert, values)
-            .then(result => {
-                response.send({
-                    message: "Password has been Changed",
-                    sucess: true
-                })
-            }).catch(err => {
                 response.status(400).send({
-                    message: "SQL Error",
-                    error: err
-                })
-            })
-        }
+                    message: 'Credentials did not match'
+                }) 
+            }
+        })
     }
 )
-
 module.exports= router
