877385f81366d7ab21623f0e60e8cf1981efea57
GyubeomKim <gyubeom@uw.edu>
2020-12-07 19:44:01 -0800

    edit password

--- routes/password.js
+++ routes/password.js
@@ -54,7 +54,7 @@ router.post('/change', (request, response, next)=> {
                 .then(result => {
                     if(result.rowCount > 0) {
                         response.json({
-                        sucess: true,
+                        success: true,
                         message: "New Password is UPDATED!"
                         })
                     } else {
@@ -66,7 +66,7 @@ router.post('/change', (request, response, next)=> {
             } else {
                 response.json({
                     success: false,
-                    message: 'Your Old Password is WRONG!'
+                    message: 'Your Old Password is WRONG (Not Match With Our System)!'
                 }) 
             }
         })
