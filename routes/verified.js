6c5d11ae9f68f742cf0b75290d49ffcf243101f5
D. Jared <idledj@uw.edu>
2020-12-09 21:51:14 -0800

    wow i just forgot a semicolon in the query

--- routes/verified.js
+++ routes/verified.js
@@ -11,7 +11,7 @@ router.get("/", (request, res) => {
     var address = request.query.email 
     var hash = request.query.hash
     // check for hash in the table
-    let theQuery = "SELECT * FROM Valid_Verifiers WHERE mHash=" + hash
+    let theQuery = "SELECT * FROM Valid_Verifiers WHERE mHash=" + hash +";"
     var isInTable = true;
     pool.query(theQuery)
             .then(result => {
@@ -55,7 +55,7 @@ router.get("/", (request, res) => {
             })
         if(isInTable) {
             console.log("mHash=" + hash)
-            let theQuery2 = "DELETE FROM Valid_Verifiers WHERE mHash=" +hash+" AND Email=" +address
+            let theQuery2 = "DELETE FROM Valid_Verifiers WHERE mHash=" +hash+" AND Email=" +address + ";"
             let values = [hash, address]
             console.log(theQuery2)
             pool.query(theQuery2, values)
