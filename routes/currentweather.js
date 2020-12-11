dfe66fa902bb8ed9a54831a6a08e1570e55ac1f7
mendei <mendei@uw.edu>
2020-12-10 19:08:48 -0800

    fresh api key for demo

--- routes/currentweather.js
+++ routes/currentweather.js
@@ -26,7 +26,7 @@ router.post("/", (req, res) => {
     
     const query = req.body.locationkey
 
-    let url = "http://dataservice.accuweather.com/currentconditions/v1/"+query+"?apikey="+API_KEY
+    let url = "http://dataservice.accuweather.com/currentconditions/v1/"+query+"?apikey="+API_KEY2
 
     request(url, function (error, response, body) {
 
