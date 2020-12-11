cf99a36f45f8807dd703efdb2edd02714bb10d5d
D. Jared <idledj@uw.edu>
2020-12-10 17:02:38 -0800

    app error fix for forgotpw.js

--- routes/forgotpw.js
+++ routes/forgotpw.js
@@ -9,7 +9,7 @@ const bodyParser = require("body-parser")
 let getHash = require('../utilities/utils').getHash
 let sendEmail = require('../utilities/utils').sendEmail
 router.use(bodyParser.json())
-router.get("/", (request, response) => {
+router.get("/", (request, res) => {
     // created group email for mailing purposes. mail sent from there
     var address = request.query.email
     if(request.query.email) {
@@ -22,19 +22,23 @@ router.get("/", (request, response) => {
                         message: "User Information not found"
                     })
                     return
-                } 
-                let newSalt = crypt.randomBytes(32).toString('hex')
-                let mySaltNewPass = getHash("randomPassword", newSalt) //hash for new password
-                let update =`UPDATE Members 
-                             SET Password=$1, Salt=$2 WHERE email=$3`
-                let vals = [mySaltNewPass, newSalt, address]
-                pool.query(update, vals)
-                        .then(result => {
-                            let message1 = "Your password with Group 1's app has been changed." +
-                            " Your new password is:\n" + mySaltNewPass
-                    sendEmail(sourceEmail, email, "changed", message1)
-                })
-                
+                }  else {
+                    let newSalt = crypt.randomBytes(32).toString('hex')
+                    let mySaltNewPass = getHash("randomPassword", newSalt) //hash for new password
+                    let update =`UPDATE Members 
+                                SET Password=$1, Salt=$2 WHERE email=$3`
+                    let vals = [mySaltNewPass, newSalt, address]
+                    pool.query(update, vals)
+                            .then(result => {
+                                let message1 = "Your password with Group 1's app has been changed." +
+                                " Your new password is:\n" + mySaltNewPass
+                        sendEmail(sourceEmail, email, "changed", message1)
+                    })
+                    res.status(201).send( {
+                        success: true,
+                        message: "Temporary password created"
+                    })
+                }
             })
             return 
     }
