1f98dc4052661fffbd7644fbb11b689e45c96352
mendei <mendei@uw.edu>
2020-12-10 19:11:49 -0800

    fresh api key for demo

--- routes/fiveday.js
+++ routes/fiveday.js
@@ -1,6 +1,7 @@
 const API_KEY = process.env.ACCUWEATHER_API_KEY
 const API_KEY2 = 'KrNJ7epExtHswickfnlR95tqZGTQkQ46'
 const API_KEY3 = 'sGtmTVEQIFpRnH0fOhfXi77X3MMlhNak'
+const API_KEY4 = 'bMpjOJLDdzSHqmrJzs5Oj33R9X4OTETn'
 
 //express is the framework we're going to use to handle requests
 const express = require('express')
@@ -26,7 +27,7 @@ router.post("/", (req, res) => {
 
     const query = req.body.locationkey
 
-    let url = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/"+query+"?apikey="+API_KEY
+    let url = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/"+query+"?apikey="+API_KEY4
 
     request(url, function (error, response, body) {
 
