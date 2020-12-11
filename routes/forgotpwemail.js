d263264c15dd006dc633eb2e97cfb5a23d6e0b3f
D. Jared <idledj@uw.edu>
2020-12-10 17:25:08 -0800

    Functional temp password creation

--- routes/forgotpwemail.js
+++ routes/forgotpwemail.js
@@ -12,7 +12,6 @@ router.get("/", (request, response) => {
     // created group email for mailing purposes. mail sent from there
     
     var address = request.query.email
-    //if(request.query.email) {
     //secure but not in terms of malicious inconvenience. there's a better way, this is too simple
      let url = 'https://mobileapp-group-backend.herokuapp.com/forgotpw?email=\''
                      + address + '\''
@@ -21,13 +20,10 @@ router.get("/", (request, response) => {
          var finalConcat1 = htmlString1.concat(url)
          var fullMessage = finalConcat1.concat(htmlString2)
          sendEmail(sourceEmail, address, "recovery", fullMessage) 
-    //}
+    
      response.send( {
          success : true,
-    //     address : address,
-    //     url : url,
          message: "Password change email sent!",
-    //     sourceEmail: sourceEmail
      })
 })
 
