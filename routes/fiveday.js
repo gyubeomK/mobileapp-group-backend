322d9c3d57d40945a287d180027e20745c25c7c6
mendei <mendei@uw.edu>
2020-12-10 21:05:59 -0800

    fresh api key for demo

--- routes/fiveday.js
+++ routes/fiveday.js
@@ -28,7 +28,7 @@ router.post("/", (req, res) => {
 
     const query = req.body.locationkey
 
-    let url = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/"+query+"?apikey="+API_KEY5
+    let url = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/"+query+"?apikey="+API_KEY2
 
     request(url, function (error, response, body) {
 
