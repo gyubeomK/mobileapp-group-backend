fee0e24ed8a68d64884ac722081237f6f93eba16
GyubeomKim <gyubeom@uw.edu>
2020-12-10 22:27:57 -0800

    edit

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -1,7 +1,7 @@
 define({
-  "name": "Lab 4",
+  "name": "Group 1",
   "version": "0.1.0",
-  "description": "The Lab 4 API Documentation",
+  "description": "Mobile App API Documentation",
   "title": "Lab 4 API Doc",
   "url": "https://mobileapp-group-backend.herokuapp.com",
   "order": [
@@ -17,7 +17,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-11-20T02:35:01.042Z",
+    "time": "2020-12-11T06:27:31.319Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
