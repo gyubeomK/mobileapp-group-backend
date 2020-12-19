97ffb37ddb5e13eaeb9c972192eafe6133118f13
Tai Nguyen <tainguyen2101@gmail.com>
2020-12-19 14:42:15 -0800

    update getAll API

--- routes/contact.js
+++ routes/contact.js
@@ -596,7 +596,7 @@ router.get("/all", (request, response, next) => {
     }
 }, (request, response) => {
     //Get contact info
-    let query = 'SELECT Members.MemberID, Members.FirstName, Members.LastName, Members.email, Members.Username FROM Members OUTER JOIN Contacts ON Members.MemberID = Contacts.MemberID_A WHERE Contacts.MemberID_A = $1'
+    let query = 'SELECT Members.MemberID, Members.FirstName, Members.LastName, Members.email, Members.Username FROM Members INNER JOIN Contacts ON Members.MemberID = Contacts.MemberID_A WHERE Contacts.MemberID_A = $1 AND Members.MemberID NOT IN (Contacts.MemberID_B)'
     let values = [request.decoded.memberid]
     pool.query(query, values)
         .then(result => {
